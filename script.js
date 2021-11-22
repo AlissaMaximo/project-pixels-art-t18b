a08a373951c8487346592ebb283fc223a9019839
AlissaMaximo <alissahiratamaximo@gmail.com>
2021-11-22 19:16:03 -0300

    Criada a função select().

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function select() {
-    
+    var oldSelected = document.getElementByClass("selected");
+    var currentSelected = document.this;
+
+    oldSelected.classList.remove("selected");
+
+    currentSelected.classList.add("selected");
 }
 
 document.getElementByClass("cor1").onload = select()
