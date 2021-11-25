b410d4fcc58fd4eaf706613a99ee1d3e757a4dd3
AlissaMaximo <alissahiratamaximo@gmail.com>
2021-11-25 14:03:40 -0300

    Todos os requisitos até (incluindo) o 7 foram atendidos.

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 window.onload = () => {
-  function select() {
-    let oldSelected = document.getElementsByClass("selected");
-    let currentSelected = event.target;
+/* Deixando a cor de fundo de todos os pixels branca. */
+  let allPixels = document.getElementsByClassName('pixel');
+  let allPixelsLength = allPixels.length;
+  for (let i = 0; i < allPixelsLength; i += 1) {
+    allPixels[i].style.backgroundColor = 'white';
+  }
+  
+/* Definindo a cor preta como cor inicial. */
+  let blackColor = document.getElementsByClassName('cor1');
+  blackColor[0].classList.add('selected');
 
-    if (oldSelected != null) {
-      oldSelected.classList.remove("selected");
-    }
-    currentSelected.classList.add("selected");
+/* Ao clicar numa cor ela é selecionada. */
+  function selectColor (e) {
+    let oldSelected = document.getElementsByClassName('selected')[0];
+    let currentSelected = e.target;
+
+    oldSelected.classList.remove('selected');
+    currentSelected.classList.add('selected');
   }
 
-  function color() {
-    let color = document.getElementByClass("selected").style.backgroundColor;
-
-    event.target.style.backgroundColor = color;
-  }
-  /* Definindo a cor preta como cor inicial */
-  document.body.getElementsByClass("cor1") = select();
-
+  document.getElementById('color-palette').addEventListener('click', selectColor);
 
 }
